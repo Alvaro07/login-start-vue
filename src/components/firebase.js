19d49c10b8e998d72edcd341689f3bf5a3c533ea
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2019-10-30 15:29:54 +0100

    firebase login and register logic

---
+++ src/components/firebase.js
@@ -0,0 +1,94 @@
+import app from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firebase-firestore'
+import 'firebase/firebase-storage'
+
+const config = {
+  apiKey: 'AIzaSyAE_4rAo3jQ-nIynUYWbDy1SJOdfCYWOV0',
+  authDomain: 'login-start-vue.firebaseapp.com',
+  databaseURL: 'https://login-start-vue.firebaseio.com',
+  projectId: 'login-start-vue',
+  storageBucket: 'login-start-vue.appspot.com',
+  appId: '1:326552357673:web:48812b452a5af34d28c538'
+}
+
+class Firebase {
+  constructor () {
+    app.initializeApp(config)
+    this.auth = app.auth()
+    this.db = app.firestore()
+    this.storage = app.storage()
+  }
+
+  /**
+   *
+   * Login function
+   * @param {string} email - The user email.
+   * @param {string} password - The user password.
+   */
+
+  login (email, password) {
+    return new Promise((resolve, reject) => {
+      this.auth
+        .signInWithEmailAndPassword(email, password)
+        .then(() => resolve())
+        .catch(error => reject(error))
+    })
+  }
+
+  /**
+   *
+   * Check that user exists
+   * @param {string} userName - The user name.
+   */
+
+  userExists (userName) {
+    return new Promise(resolve => {
+      this.db
+        .collection('users')
+        .doc(userName)
+        .get()
+        .then(doc => resolve(doc))
+    })
+  }
+
+  /**
+   *
+   * Register function
+   * @param {string} email - The user email.
+   * @param {string} password - The user password.
+   * @param {string} userName - The user name.
+   */
+
+  register (email, password, userName) {
+    return new Promise((resolve, reject) => {
+      this.auth
+        .createUserWithEmailAndPassword(email, password)
+        .then(() => {
+          this.db
+            .collection('users')
+            .doc(userName)
+            .set({
+              displayName: userName,
+              email: email
+            })
+            .then(() => {
+              this.auth.currentUser.updateProfile({ displayName: userName }).then(() => resolve())
+            })
+            .catch(error => error)
+        })
+        .catch(error => reject(error))
+    })
+  }
+
+  /**
+   *
+   * Logout function
+   */
+
+  logout () {
+    return this.auth.signOut()
+  }
+}
+
+export default new Firebase()
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,20 +4,46 @@
     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam leo diam, scelerisque a eleifend sed."
     type="register"
     @onSubmit="data => handleSubmit(data)"
+    :error="error"
+    :loading="loading"
   />
 </template>
 
 <script>
 import UserForm from "@/components/UserForm";
+import firebase from "@/components/firebase";
 
 export default {
   name: "register",
   components: {
     UserForm
   },
+  data() {
+    return {
+      error: "",
+      loading: false
+    };
+  },
   methods: {
     handleSubmit(data) {
-      console.log(data);
+      this.loading = true;
+
+      firebase.userExists(data.userName).then(doc => {
+        if (doc.exists) {
+          this.error = "the user already exists";
+        } else {
+          firebase
+            .register(data.email, data.password, data.userName)
+            .then(() => {
+              this.loading = false;
+              this.$router.push("/");
+            })
+            .catch(error => {
+              this.loading = false;
+              this.error = error.message;
+            });
+        }
+      });
     }
   }
 };
